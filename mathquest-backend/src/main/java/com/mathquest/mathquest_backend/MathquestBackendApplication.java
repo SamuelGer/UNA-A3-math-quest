f8cb1756300095729a9082294c5bcafafaf5f58b
SamuelGer <samuelgercossimo2007@gmail.com>
2026-05-19 17:18:30 -0300

    Correção

--- mathquest-backend/src/main/java/com/mathquest/mathquest_backend/MathquestBackendApplication.java
+++ mathquest-backend/src/main/java/com/mathquest/mathquest_backend/MathquestBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class cdMathquestBackendApplication {
+public class MathquestBackendApplication {
 	static void main(String[] args) {
 		SpringApplication.run(MathquestBackendApplication.class, args);
 	}
