3addf1acbf911d04070cce043f4924a67af9d5ad
SamuelGer <samuelgercossimo2007@gmail.com>
2026-05-22 12:50:10 -0300

    Corsfilter com maior prioridade

--- mathquest-backend/src/main/java/com/mathquest/mathquest_backend/config/CorsConfig.java
+++ mathquest-backend/src/main/java/com/mathquest/mathquest_backend/config/CorsConfig.java
@@ -1,21 +1,40 @@
 package com.mathquest.mathquest_backend.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        //Permite envio de cookies ou credenciais de sessão. (se for necessário)
+        config.setAllowCredentials(true);
+        //Vai definir e permitir as origens abaixo
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:5000",
+                "http://127.0.0.1:5000",
+                "https://mathquest-nu.vercel.app"));
+        // Permite todos os headers comuns e os customizados
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        // Garante uso de todos os métodos http abaixo
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // Aplica essa mesma config pra todas as rotas
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
 
-    @Value("${FRONTEND_URL:http://localhost:3000}")
-    private String FRONTEND_URL;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5000", "http://127.0.0.1:5500", FRONTEND_URL)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
     }
+
+
+
+
+
 }
